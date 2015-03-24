7cada68f0c4a8a51e9ea6a657f32e692845336da
palaster <palaster307@gmail.com>
2015-03-24 17:39:21 -0500

    Finished Core Fuel
    -Finished core fuel.

--- src/main/java/palaster97/ss/items/ItemCoreFuel.java
+++ src/main/java/palaster97/ss/items/ItemCoreFuel.java
@@ -1,6 +1,8 @@
 package palaster97.ss.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class ItemCoreFuel extends ItemModSpecialNBT {
 
@@ -14,4 +16,15 @@ public class ItemCoreFuel extends ItemModSpecialNBT {
 	
 	@Override
 	public Item getContainerItem() { return this; }
+	
+	@Override
+	public ItemStack getContainerItem(ItemStack itemStack) {
+		ItemStack stack = new ItemStack(getContainerItem());
+		stack.setTagCompound(new NBTTagCompound());
+		if(itemStack.hasTagCompound()) {
+			stack.getTagCompound().setString("PlayerUUID", itemStack.getTagCompound().getString("PlayerUUID"));
+			return stack;
+		}
+		return super.getContainerItem(itemStack);
+	}
 }
